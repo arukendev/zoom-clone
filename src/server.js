7961a5a2377d3b4efd266d0e00215ec36c5ad63e
youngkaya <iamjaehyun4@gmail.com>
2022-09-06 21:18:20 +0900

    connect server and browser

--- src/server.js
+++ src/server.js
@@ -7,11 +7,21 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000/`);
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+wss.addListener("connection", (socket) => {
+    console.log("Connected to Browser ✅");
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+    socket.on("message", (message) => {
+        console.log(message + "");
+    });
+    socket.send("hello!!");
+});
 
 server.listen(3000, handleListen);
